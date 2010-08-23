6a842dd9059ff87f283d989d1630382b51e6fd67
unknown <Vultaire@.(none)>
2010-08-23 23:10:21 +0900

    Added prelim table code.

--- jblite/jmdict_proto2.py
+++ jblite/jmdict_proto2.py
@@ -6,6 +6,119 @@ from cStringIO import StringIO
 from xml.etree.cElementTree import ElementTree
 
 
+
+class Table(object):
+
+    """Base class for tables."""
+
+    # These queries must be specified in child classes.
+    table_name = None
+    create_query = None
+    insert_query = None
+    index_queries = []
+
+    def __init__(self, cursor):
+        self.cursor = cursor
+        self.__next_id = 1
+
+    def create(self):
+        """Creates table, plus indices if supplied in class definition."""
+        self.cursor.execute(self._get_create_query())
+        index_queries = self._get_index_queries()
+        for query in index_queries:
+            self.cursor.execute(query)
+
+    def insert(self, *args):
+        self.cursor.execute(self._get_insert_query(), args)
+
+    def _get_create_query(self):
+        if self.table_name is None:
+            raise ValueError(
+                "table_name must be specified in class definition")
+        return self.create_query % self.table_name
+
+    def _get_insert_query(self):
+        if self.table_name is None:
+            raise ValueError(
+                "table_name must be specified in class definition")
+        return self.insert_query % self.table_name
+
+    def _get_index_queries(self):
+        if self.table_name is None:
+            raise ValueError(
+                "table_name must be specified in class definition")
+        if (not (isinstance(self.index_queries, list))
+          or (len(self.index_queries) == 0)):
+            return []
+        else:
+            queries = [q % self.table_name for q in self.index_queries]
+            return queries
+
+
+class AutoIncrementTable(Table):
+
+    """Auto-increment table base class."""
+
+    def __init__(self, cursor):
+        Table.__init__(self, cursor)
+        self.__next_id = 1
+
+    def insert(self, *args):
+        i = self._get_id()
+        query_args = [i] + args
+        try:
+            self.cursor.execute(self._get_insert_query(),
+                                query_args)
+        except Exception, e:
+            self._rollback_id()
+            raise e
+
+    def _get_id(self):
+        """Gets next auto-increment ID."""
+        result = self.next_id
+        self.__next_id += 1
+        return result
+
+    def _rollback_id(self):
+        """Rolls back ID (in case of errors)"""
+        self.__next_id -= 1
+
+    def _peek_next_id(self):
+        """Currently unused."""
+        # Here only to complete the API.
+        return self.__next_id
+
+
+class EntityRefTable(AutoIncrementTable):
+    # Used for: ke_inf, re_inf, dial, 3 others...
+    table_name = None
+    # i = auto-inc, fk = foreign key, entity = value
+    create_query = "CREATE TABLE %s (i INTEGER, fk INTEGER, entity INTEGER)"
+    insert_query = "INSERT INTO %s VALUES (?, ?, ?)"
+    index_queries = [
+        "CREATE INDEX fk_index ON %s (fk)",
+        ]
+
+# ----- Real tables follow -----
+
+class KeInfTable(EntityRefTable):
+    table_name = "ke_inf"
+class ReInfTable(EntityRefTable):
+    table_name = "re_inf"
+class DialectTable(EntityRefTable):
+    table_name = "dial"
+
+
+class EntityTable(AutoIncrementTable):
+    table_name = "entities"
+    create_query = "CREATE TABLE %s (i INTEGER, entity TEXT, expansion TEXT)"
+    insert_query = "INSERT INTO %s VALUES (?, ?, ?)"
+    index_queries = [
+        "CREATE INDEX entity_index ON %s (entity)",
+        ]
+
+
+
 def get_dtd(xml_data):
     """Gets the DTD from JMdict."""
     # This works for JMdict (as it is at the time of writing), but is
