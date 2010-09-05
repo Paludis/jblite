d8fcc6c7a448d92160c7611a5b608a54c6aea2c9
unknown <Vultaire@.(none)>
2010-09-05 18:05:27 +0900

    Started to produce top-down code for JMdict SQLite3 database.

--- jblite/helpers.py
+++ jblite/helpers.py
@@ -1,4 +1,5 @@
-import sqlite3
+import sqlite3, gzip
+
 
 def with_db(db_fname, fn, *args, **kwargs):
     """Wrapper for a full self-contained SQLite database transaction."""
@@ -10,3 +11,18 @@ def with_db(db_fname, fn, *args, **kwargs):
     finally:
         cur.close()
         conn.close()
+
+
+def gzread(fname):
+    try:
+        infile = gzip.open(fname)
+        data = infile.read()
+        infile.close()
+    except IOError, e:
+        if e.args[0] == "Not a gzipped file":
+            with open(fname) as infile:
+                data = infile.read()
+        else:
+            raise e
+    return data
+
--- jblite/jmdict_proto2.py
+++ jblite/jmdict_proto2.py
@@ -1,9 +1,17 @@
 from __future__ import print_function
 from __future__ import with_statement
 
-import sys, gzip, re
+import os, sys, re, sqlite3
 from cStringIO import StringIO
 from xml.etree.cElementTree import ElementTree
+from helpers import gzread
+
+import gettext
+#t = gettext.translation("jblite")
+#_ = t.ugettext
+gettext.install("jblite")
+
+
 
 
 """
@@ -16,6 +24,79 @@ $ grep -e '&.*;' JMdict | grep -v -e '&lt;' -e '&gt;' -e '&nbsp;' -e '&amp;' | s
 <re_inf>
 """
 
+
+
+class Database(object):
+
+    def __init__(self, filename, init_from_file=None):
+        self.conn = sqlite3.connect(filename)
+        self.cur = self.conn.cursor()
+        if init_from_file is not None:
+            self._init_from_file(init_from_file)
+
+    def search(self, query, pref_lang=None):
+        raise NotImplementedError()
+
+    def _init_from_file(self, jmdict_src):
+        raw_data = gzread(jmdict_src)
+        entities = self._get_entities(raw_data)
+
+        infile = StringIO(raw_data)
+        etree = ElementTree(file=infile)
+        infile.close()
+
+        self._process_etree(etree, entities)
+
+    def _get_entities(self, xml_data):
+        """Gets the ENTITY definitions from JMdict.
+
+        Finds the built-in DTD and extracts all ENTITY definitions.
+
+        """
+        dtd = self._get_dtd(xml_data)
+        # do some logic to find all entities...
+        entities = {}
+        regex = "<!ENTITY[ ]+([a-zA-Z-]+)[ ]+['\"](.*?)['\"]>"
+        for match in re.finditer(regex, xml_data):
+            key, value = match.groups()[0:2]
+            key = "&%s;" % key  # Convert to &entity; format
+            entities[key] = value
+        return entities
+
+    def _get_dtd(self, xml_data):
+        """Gets the DTD from JMdict."""
+        # This works for JMdict (as it is at the time of writing), but is
+        # not a general solution.
+        start_index = xml_data.find("<!DOCTYPE")
+        if start_index == -1:
+            raise Exception("Could not find start of internal DTD")
+        end_index = xml_data.find("]>")
+        if end_index == -1:
+            raise Exception("Could not find end ofinternal DTD")
+        end_index += 2
+        dtd = xml_data[start_index:end_index]
+        return dtd
+
+    def _process_etree(self, etree, entities):
+        for i, elem in enumerate(etree.getiterator("entry")):
+            if i >= 10:
+                break
+            print(i, elem)
+
+
+
+"""
+Creating entries:
+
+1. Need objects from DB
+2. Need to parse XML and:
+   1. Directly create DB
+   2. Create entry objects, and create DB from them.
+      - Strength: allows opportunity for modifying database in the future.
+      - Weakness: Need two ways to create objects: from DB and from
+        SQL.  This complicates things...
+
+"""
 
 
 class Table(object):
@@ -129,65 +210,10 @@ class EntityTable(AutoIncrementTable):
         ]
 
 
-
-def get_dtd(xml_data):
-    """Gets the DTD from JMdict."""
-    # This works for JMdict (as it is at the time of writing), but is
-    # not a general solution.
-    start_index = xml_data.find("<!DOCTYPE")
-    if start_index == -1:
-        raise Exception("Could not find start of internal DTD")
-    end_index = xml_data.find("]>")
-    if end_index == -1:
-        raise Exception("Could not find end ofinternal DTD")
-    end_index += 2
-    dtd = xml_data[start_index:end_index]
-    return dtd
-
-def get_entities(xml_data):
-    """Gets the ENTITY definitions from JMdict.
-
-    Finds the built-in DTD and extracts all ENTITY definitions.
-
-    """
-    dtd = get_dtd(xml_data)
-    # do some logic to find all entities...
-    entities = {}
-    regex = "<!ENTITY[ ]+([a-zA-Z-]+)[ ]+['\"](.*?)['\"]>"
-    for match in re.finditer(regex, xml_data):
-        key, value = match.groups()[0:2]
-        key = "&%s;" % key  # Convert to &entity; format
-        entities[key] = value
-    return entities
-
-def gzread(fname):
-    try:
-        infile = gzip.open(sys.argv[1])
-        data = infile.read()
-        infile.close()
-    except IOError, e:
-        if e.args[0] == "Not a gzipped file":
-            with open(sys.argv[1]) as infile:
-                data = infile.read()
-        else:
-            raise e
-    return data
-
-def parse_file(fname):
-    """Loads file (gzipped or not) and returns parsed data.
-
-    Result is a two-item tuple: (ElementTree, entities (dict))
-
-    """
-    raw_data = gzread(fname)
-    entities = get_entities(raw_data)
-    infile = StringIO(raw_data)
-    etree = ElementTree(file=infile)
-    infile.close()
-    return (etree, entities)
-
 def main():
-    entities, etree = parse_file(sys.argv[1])
+    if len(sys.argv) < 3:
+        print(_("Please specify"), file=sys.stderr)
+    db = Database(sys.argv[1], init_from_file=sys.argv[2])
 
 if __name__ == "__main__":
     main()
