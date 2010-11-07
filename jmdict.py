88319a524380577f6f78f75fbd1f2c245f4d40c4
Paul Goins <general@vultaire.net>
2010-11-07 22:29:34 +0900

    Added convenience scripts.

---
+++ jmdict.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+"""Front-end script to run JMdict sub-app."""
+
+from jblite import jmdict
+jmdict.main()
---
+++ kd2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+"""Front-end script to run KD2 sub-app."""
+
+from jblite import kd2
+kd2.main()
