e66b9a12e736aac7e6361dc8fa97d7e21ee440fb
Paul Goins <general@vultaire.net>
2010-12-07 23:36:10 +0900

    Version up.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='jblite',
-      version='0.4',
+      version='0.4.1',
       description='J-Ben SQLite parsing scripts',
       author='Paul Goins',
       author_email='general@vultaire.net',
